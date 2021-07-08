6bee76cbd938cf47859c3a6b46bd59870c28db3c
SolletiHarshitha <solletiharshitha@gmail.com>
2021-07-08 12:25:47 +0530

    [Harshitha] Choosing Input value

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game");
+           TicTacToeGame uc1=new TicTacToeGame();
+            uc1.CreateBoard();
+            uc1.ToStartGame();
+            uc1.ChooseXorO();
+        }
+        
+    }
+}
---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        public char [] board;
+        public  char[] CreateBoard()
+        {
+            board = new char[10];
+            for(int i = 1; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+        public void ToStartGame()
+        {
+            Random random = new Random();
+            int player = random.Next(0, 2);
+            if(player==1)
+            {
+                Console.WriteLine("Player starts a game");
+
+            }
+            else
+            {
+                Console.WriteLine("Computer starts a game");
+            }
+        }
+        public void ChooseXorO()
+        {
+            Console.WriteLine("Choose a letter either X or O");
+            char playerLetter = Convert.ToChar(Console.ReadLine());
+            char computerLetter=' ';
+            if (playerLetter.Equals('X'))
+            {
+                computerLetter = 'O';
+            }
+            else
+            {
+                computerLetter = 'X';
+            }
+            Console.WriteLine("Player has choosen : "+playerLetter);
+            Console.WriteLine("Computer has choosen: "+computerLetter);
+            
+        }
+    }
+}
