8d40a4cee07241927f54d2d13cd429c8feacd02b
SolletiHarshitha <86297009+SolletiHarshitha@users.noreply.github.com>
2021-07-08 19:34:55 +0530

    Update Program.cs

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -7,13 +7,18 @@ namespace TicTacToe
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to TicTacToe Game");
-           TicTacToeGame game=new TicTacToeGame();
-            char [] printBoard=game.CreateBoard();
+            TicTacToeGame game = new TicTacToeGame();
+            char[] printBoard = game.CreateBoard();
+
             char playerValue = game.GetPlayerInput();
-            char computerValue=game.GetComputerInput(playerValue);
-            Console.WriteLine("Computer value is :"+computerValue+"\n");
-            
-            
+            char computerValue = game.GetComputerInput(playerValue);
+            Console.WriteLine("Computer value is :" + computerValue + "\n");
+
+            game.DisplayBoard();
+
+            Console.WriteLine("Enter position value to insert element : ");
+            int position = Convert.ToInt32(Console.ReadLine());
+            game.GetPosition(position,playerValue);
         }
         
     }
